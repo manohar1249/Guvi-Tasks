7eab9bf4da88e6bf90312df6abdefde1544ad5ac
Mark85 <krishnakiriti.mamidi@gmail.com>
2020-09-08 21:33:29 +0530

    Added error handling to XMLHttpRequest

--- WeatherAPI/weatherApi.js
+++ WeatherAPI/weatherApi.js
@@ -11,14 +11,23 @@ request.open('GET',url,true)
 request.send()
 
 request.onload = function(){
+	if(request.readyState == 4 && request.status == 200){
 	var data = JSON.parse(this.response);
-	var temp = (data["main"]["temp"]-273.15).toFixed(1
-		);
+	var temp = (data["main"]["temp"]-273.15).toFixed(1);
 	document.querySelector('#weather').innerHTML = data["name"]+","+data["sys"]["country"]+" Temperature "+temp+"&deg, wind "+data["wind"]["speed"] +" m/s, pressure " +data["main"]["pressure"]+" hpa";
-	//console.log(data);
 	console.log(city,country);	
-	
+	}
+	else{
+		alert("Status: "+request.status+" Message: "+request.statusText);
+		console.log("Status: "+request.status+" Message: "+request.statusText);
+	}
 }
+	
+request.onerror = function(){
+	console.log("connection failed")
+	alert("Connection failed");
+}
+	
 
 }
 
